d94a3df11e4504db160fc2c2cc3013fcf352fde2
shacorne <shacorne23@gmail.com>
2015-10-08 18:30:35 +0000

    Added nickname

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a complete ninja now');
 });
 
-app.listen(process.env.PORT || 4000);
+app.get('/nickname', function(request, response){
+    response.send('brainy-dinosaur');
+});
+
+app.listen(process.env.PORT || 4000, process.env.IP || 'localhost');
